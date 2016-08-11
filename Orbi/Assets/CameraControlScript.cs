38c97c86ab5929c211dc63ac4a771f6de534bb10
Lucas Reeh <lr86gm@gmail.com>
2016-08-11 18:47:23 +0200

    [ORBI-1] added scifi create assets

--- Orbi/Assets/CameraControlScript.cs
+++ Orbi/Assets/CameraControlScript.cs
@@ -12,9 +12,16 @@ public class CameraControlScript : MonoBehaviour
     private float yAngTemp = 0.0F;
     public float speed = 100.0F;
 
+    // gyro
+    private float initialYAngle = 0f;
+    private float appliedGyroYAngle = 0f;
+    private float calibrationYAngle = 0f;
+
     // Use this for initialization
     void Start()
     {
+        Application.targetFrameRate = 60;
+        initialYAngle = transform.eulerAngles.y;
         xAngle = 0.0F;
         yAngle = 0.0F;
         this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0F);
@@ -24,7 +31,8 @@ public class CameraControlScript : MonoBehaviour
     {
         touchCameraRotation();
         keyCameraRotation();
-        
+        ApplyGyroRotation();
+        ApplyCalibration();
     }
 
     void keyCameraRotation()
@@ -74,5 +82,31 @@ public class CameraControlScript : MonoBehaviour
                 this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0F);
             }
         }
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Calibrate", GUILayout.Width(300), GUILayout.Height(100)))
+        {
+            CalibrateYAngle();
+        }
+    }
+
+    public void CalibrateYAngle()
+    {
+        calibrationYAngle = appliedGyroYAngle - initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+    }
+
+    void ApplyGyroRotation()
+    {
+        transform.rotation = Input.gyro.attitude;
+        transform.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
+        transform.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device.
+        appliedGyroYAngle = transform.eulerAngles.y; // Save the angle around y axis for use in calibration.
+    }
+
+    void ApplyCalibration()
+    {
+        transform.Rotate(0f, -calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
     }
 }
--- Orbi/Assets/InitScript.cs
+++ Orbi/Assets/InitScript.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Net;
 
 public class InitScript : MonoBehaviour
 {
+    public GameObject cubePrefab;
 
     void Awake()
     {
@@ -24,43 +26,62 @@ public class InitScript : MonoBehaviour
 
     public void UdpateWorld(double latitude, double longitude, double elevation)
     {
-        // remove old cubes
-        GameObject[] oldCubes = GameObject.FindGameObjectsWithTag("world_cube");
         
-        foreach (GameObject cube in oldCubes)
-        {
-            GameObject.Destroy(cube);
-        }
         Debug.Log("UpdateWorld");
-        GoogleMap map = GameObject.FindGameObjectWithTag("maps_container").GetComponent<GoogleMap>();
-        if (map)
+        if (GameObject.FindGameObjectWithTag("maps_container"))
         {
-            Debug.Log("update maps " + latitude);
-            map.centerLocation.latitude = Convert.ToSingle(latitude);
-            map.centerLocation.longitude = Convert.ToSingle(longitude);
-            map.Refresh();
+            GoogleMap map = GameObject.FindGameObjectWithTag("maps_container").GetComponent<GoogleMap>();
+            if (map)
+            {
+                Debug.Log("update maps " + latitude);
+                map.centerLocation.latitude = Convert.ToSingle(latitude);
+                map.centerLocation.longitude = Convert.ToSingle(longitude);
+                map.Refresh();
+            }
         }
+        
         Debug.Log("UpdateWorld " + latitude);
 
-        string uri = "http://localhost:8080/api/world";
+        string uri = "https://softwaredesign.foundation/orbi/api/world";
         uri = uri + "?";
         uri = uri + "latitude=" + latitude;
         uri = uri + "&";
         uri = uri + "longitude=" + longitude;
         uri = uri + "&user=test";
         //Debug.Log("debug = " + uri);
-        HTTP.Request someRequest = new HTTP.Request("get", uri);
-        someRequest.AddHeader("Accept", "application/json");
-        someRequest.Send((request) => {
-            // parse some JSON, for example:
-            JSONObject world = new JSONObject(request.response.Text);
+        Dictionary<string,string> headers = new Dictionary<string, string>();
+        headers.Add("Accept", "application/json");
+        WWW www = new WWW(uri, null, headers);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            JSONObject world = new JSONObject(www.text);
+            //Debug.Log("world " + world);
             if (world.type == JSONObject.Type.OBJECT)
             {
+                // remove old cubes
+                GameObject[] oldCubes = GameObject.FindGameObjectsWithTag("world_cube");
+
+                foreach (GameObject cube in oldCubes)
+                {
+                    GameObject.Destroy(cube);
+                }
                 JSONObject cubes = (JSONObject)world.list[0];
                 foreach (JSONObject cube in cubes.list)
                 {
-                    GameObject newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    //Debug.Log("adding cube " + cube);
+
+                    GameObject newCube = Instantiate(cubePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                    // Modify the clone to your heart's content
                     newCube.transform.parent = GameObject.FindGameObjectWithTag("cubes_container").transform;
+                    newCube.transform.localScale = new Vector3(0.2F, 0.2F, 0.2F);
                     newCube.tag = "world_cube";
                     newCube.name = "cube_" + cube.list[1].n;
                     newCube.transform.position = new Vector3(cube.list[0].list[0].n, cube.list[0].list[1].n, cube.list[0].list[2].n);
@@ -68,14 +89,13 @@ public class InitScript : MonoBehaviour
             }
             else
             {
-                Debug.Log("Sorry nothing here, start create your world!");
+                //Debug.Log("Sorry nothing here, start create your world!");
             }
-
-        });
-
-        
-
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
     }
-
 
 }

b1162b376054a999820a50c81c579f52516c1619
Lucas Reeh <lr86gm@gmail.com>
2016-10-09 02:06:34 +0200

    [ORBI-14] deploy settings

--- Orbi-App/Assets/Scripts/Game/Client/Client.cs
+++ Orbi-App/Assets/Scripts/Game/Client/Client.cs
@@ -10,7 +10,7 @@ namespace GameController
 
         // prod server, dev server or localhost
         public ServerType serverType = ServerType.LOCAL;
-        public static int VERSION = 13;
+        public static int VERSION = 14;
         //private AuthService authService = new AuthService();
         private int runningRequests = 0;
 

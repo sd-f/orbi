8cb27bc823a8af2578c8f0344a922ff7c380bbb1
Lucas Reeh <lucas.reeh@tugraz.at>
2016-11-23 12:51:48 +0100

    [ORBI-25] sequence renaming and fixes in jpa annotations

--- Orbi-Server/src/main/java/foundation/softwaredesign/orbi/persistence/entity/GameObjectEntity.java
+++ Orbi-Server/src/main/java/foundation/softwaredesign/orbi/persistence/entity/GameObjectEntity.java
@@ -12,8 +12,11 @@ import java.util.Date;
 public class GameObjectEntity {
 
     @Id
-    @SequenceGenerator(name="game_object_id_gen", schema = "public", sequenceName="game_object_id_gen")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "game_object_id_gen")
+    @SequenceGenerator(name="game_object_id_seq",
+            schema = "public",
+            sequenceName="game_object_id_seq", allocationSize=1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "game_object_id_seq")
+    @Column(updatable = false)
     private Long id;
     @Column
     @NotNull
